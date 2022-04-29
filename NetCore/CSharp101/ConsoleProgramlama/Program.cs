fd49d74c99ce3e4c39eb7ae020bca9753dedd953
Hakan Ozhan <ozhanhakan@gmail.com>
2022-04-29 12:08:06 +0300

    Initial commit

---
+++ NetCore/CSharp101/ConsoleProgramlama/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Bu program ortamı komut satırında 
+dotnet new console
+komutu kullanılarak otomatik başlatıldı.
+Patika\Net Core\CSharp101\ConsoleProgramlama>dotnet new console
+*/
+
+using System;
+
+namespace ConsoleProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        //static nesnesi yaratılmadan objeye ulaşmak.
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi giriniz!");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("soyisminizi giriniz!");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba!, " +name+" "+surname);
+            Console.ReadKey();
+
+            /* launch.json dosyasındaki "console": "internalConsole",
+                        "console": "integratedTerminal",
+                        ya da 
+                         "console": "externalTerminal",
+
+            olarak değiştirilmeli ki terminal okuma yapabilsin
+            */
+
+        }
+    }
+}
