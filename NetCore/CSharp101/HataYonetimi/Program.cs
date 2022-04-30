f5f5be28c69f166c34485faef27fec8281954f9d
Hakan Ozhan <ozhanhakan@gmail.com>
2022-04-30 16:39:49 +0300

    Hata yönetimi dersi
    https://app.patika.dev/courses/csharp-101/1-try-catch-finally-ve-mantiksal-hatalar

---
+++ NetCore/CSharp101/HataYonetimi/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace HataYonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool hataVar=false;
+           /*  try
+            {
+                Console.WriteLine("Bir sayı giriniz");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Girmiş olduğunuz sayı: "+sayi); 
+            }
+            catch(Exception e){
+                hataVar=true;
+                Console.WriteLine("Hata: "+e.Message.ToString());
+
+            }
+            finally
+            {
+                if(hataVar)
+                Console.WriteLine("işlem hatasız tamamlanamadı!");
+                else
+                Console.WriteLine("işlem tamamlandı.");
+            } */
+
+            try
+            {
+                int a = int.Parse("2");
+                Console.WriteLine(a);
+                Console.WriteLine(a*2);
+                int b =int.Parse("-2500000000"); 
+            }
+            catch (ArgumentNullException e)
+            {
+                hataVar=true;
+               Console.WriteLine("Boş değer girdiniz.");
+               Console.WriteLine(e);
+            }
+            catch (FormatException e)
+            {
+                hataVar=true;
+               Console.WriteLine("Veritipi uygun değil.");
+               Console.WriteLine(e);
+            }
+            catch(OverflowException e){
+                hataVar=true;
+                Console.WriteLine("Aralık dışında bir değer girildi.");
+                Console.WriteLine(e);
+            }
+
+            finally
+            {
+                if(hataVar)
+                Console.WriteLine("işlem hatasız tamamlanamadı!");
+                else
+                Console.WriteLine("işlem tamamlandı.");
+            }
+           
+        }
+    }
+}
