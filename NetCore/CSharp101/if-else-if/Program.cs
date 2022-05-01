06bf6703a4ab30a0fce43aa87da7784bd3b0e7d1
Hakan Ozhan <ozhanhakan@gmail.com>
2022-05-01 11:45:03 +0300

    if-else-if-ternary konusu
    https://app.patika.dev/courses/csharp-101/1-if-else-yapisi-ve-ternary-if

---
+++ NetCore/CSharp101/if-else-if/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***if-else-if***");
+            //saate göre selamlama
+            int time = DateTime.Now.Hour-5;
+            if(time>=6 && time<=11)
+                Console.WriteLine("if-else-if yöntemi: \n saat: "+time+" Günaydın!");
+            else if (time<=18)
+                Console.WriteLine("if-else-if yöntemi: \n saat: "+time+" iyi günler!");
+            else
+                Console.WriteLine("if-else-if yöntemi: \n saat: "+time+" iyi geceler!");
+
+            string sonuc = time >=6 && time<=11 ?  " Günaydın!" : time<=18 ? "iyi günler!":"iyi geceler!";
+            Console.WriteLine("ternary yöntemi: \n saat: "+time+" "+sonuc);
+        }
+    }
+}
