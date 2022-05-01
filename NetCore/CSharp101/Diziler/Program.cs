3d7d40b6972f2807b7c0cf29d9e18d571cee886e
Hakan Ozhan <ozhanhakan@gmail.com>
2022-05-01 16:40:00 +0300

    patika.dev C# Diziler dersinin takibi
    https://app.patika.dev/courses/csharp-101/1-dizi-tan%C4%B1mlama-eris%CC%A7im-ve-do%CC%88ngu%CC%88lerle-kullan%C4%B1m

---
+++ NetCore/CSharp101/Diziler/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dizi Tanımlama
+            string[] renkler = new string[5];
+            string[] hayvanlar = { "kedi", "köpek", "Kuş", "Aslan" };
+            int[] dizi;
+            dizi = new int[5];
+
+            //Dizilere değer atama ve erişim
+            renkler[0] = "Mavi";
+            dizi[3] = 10;
+            System.Console.WriteLine(hayvanlar[1]);
+            System.Console.WriteLine(renkler[0]);
+            Console.WriteLine(dizi[3]);
+
+            //Döngülerle dizi kullanımı
+            //dizi boyutunun console üzerinden belirlenip, 
+            //girilen n adet sayıları diziye aktarıp ortalamasını almak
+
+            System.Console.WriteLine("Lütfen dizinin eleman sayısını giriniz");
+            int diziUzunlugu = int.Parse(Console.ReadLine().Trim());
+            int[] sayiDizisi = new int[diziUzunlugu];
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz ", i + 1);
+                sayiDizisi[i] = int.Parse(Console.ReadLine().Trim());
+
+
+            }
+
+            int toplam = 0;
+            foreach (var item in sayiDizisi)
+            {
+
+                toplam+=item;
+            }
+
+            System.Console.WriteLine("Ortalama: "+toplam/diziUzunlugu);
+        }
+    }
+}
