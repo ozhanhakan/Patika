be42a3dd524fade703b23ab64f4ebb68125be572
Hakan Ozhan <ozhanhakan@gmail.com>
2022-05-01 17:27:43 +0300

    DizilerArrayMethodlar
    https://app.patika.dev/courses/csharp-101/2-array-sinifi-ve-methodlari

---
+++ NetCore/CSharp101/DizilerArrayMethodlar/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace DizilerArrayMethodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 11, 17 };
+
+            Console.WriteLine("Sırasız Dizi");
+            foreach (var item in sayiDizisi)
+            {
+                System.Console.WriteLine(item);
+            }
+            Console.WriteLine("Sıralı Dizi");
+            Array.Sort(sayiDizisi);
+            foreach (var item in sayiDizisi)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //clear
+
+            System.Console.WriteLine("Array Clear");
+            Array.Clear(sayiDizisi, 2, 2);//2.indeksten itibareb iki elemanı 0 yapar
+            foreach (var item in sayiDizisi)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //reverse
+            System.Console.WriteLine("Array Reverse");
+            Array.Reverse(sayiDizisi);
+            foreach (var item in sayiDizisi)
+            {
+                System.Console.WriteLine(item);
+            }
+
+            //indexOf
+            System.Console.WriteLine("Array indexOf");
+            
+            System.Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+            //Resize
+            System.Console.WriteLine("Array resize");
+            Array.Resize<int>(ref sayiDizisi,9);
+            sayiDizisi[8]=99;
+            foreach (var item in sayiDizisi)
+            {
+                System.Console.WriteLine(item);
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
