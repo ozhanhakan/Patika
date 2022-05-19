32b25f1f9fbff8cdfef99c958fab3cb4d587e43b
Hakan Ozhan <ozhanhakan@gmail.com>
2022-05-19 17:27:24 +0300

    EnumKavramı ders takibi
    https://app.patika.dev/courses/csharp-101/17-enum

---
+++ NetCore/CSharp101/EnumKavrami/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EnumKavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Cuma);
+            Console.WriteLine((int)Gunler.Cuma);
+            int sıcaklık = 32;
+            if (sıcaklık <= (int)HavaDurumu.Normal)
+            {
+                System.Console.WriteLine("Dışarıya çıkmak için Havanın ısınmasını bekleyelim");
+
+            }
+            else if (sıcaklık >= (int)HavaDurumu.Sıcak)
+            {
+                System.Console.WriteLine("Dışarıya çıkmak için çok sıcak bir gün!");
+            }
+            else if (sıcaklık >= (int)HavaDurumu.Normal && sıcaklık < (int)HavaDurumu.ÇokSıcak)
+            {
+                System.Console.WriteLine("Hadi dışarıya çıkalım.");
+            }
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi = 1,
+        Salı,
+        Çarşamba = 20,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soğuk = 5,
+        Normal = 20,
+        Sıcak = 25,
+        ÇokSıcak = 30
+    }
+}
